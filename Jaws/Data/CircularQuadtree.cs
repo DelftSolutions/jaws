d7fe22b0a6a0ffc971384e8d21a00cee90a5ab6a
Max Maton <thexa4@gmail.com>
2013-03-05 15:14:04 +0100

    Implemented GetArea

--- Jaws/Data/CircularQuadtree.cs
+++ Jaws/Data/CircularQuadtree.cs
@@ -14,18 +14,69 @@ namespace Jaws.Data
         protected Dictionary<T, CircularQuadNode> nodes;
 
         /// <summary>
-        /// Creates a new CircularQuadTree
+        /// Creates a new CircularQuadTree (3d representation)
         /// </summary>
         public CircularQuadtree(T face1, T face2, T face3, T face4, T face5, T face6)
         {
+            var f1 = new CircularQuadNode();
+            var f2 = new CircularQuadNode();
+            var f3 = new CircularQuadNode();
+            var f4 = new CircularQuadNode();
+            var f5 = new CircularQuadNode();
+            var f6 = new CircularQuadNode();
+
+            f1.Value = face1;
+            f2.Value = face2;
+            f3.Value = face3;
+            f4.Value = face4;
+            f5.Value = face5;
+            f6.Value = face6;
+
+            f1.NeighbourUp = f2;
+            f2.NeighbourUp = f3;
+            f3.NeighbourUp = f4;
+            f4.NeighbourUp = f5;
+            f5.NeighbourUp = f6;
+            f6.NeighbourUp = f1;
+
+            f1.NeighbourDown = f4;
+            f2.NeighbourDown = f6;
+            f3.NeighbourDown = f2;
+            f4.NeighbourDown = f1;
+            f5.NeighbourDown = f3;
+            f6.NeighbourDown = f5;
+
+            f1.NeighbourLeft = f6;
+            f2.NeighbourLeft = f5;
+            f3.NeighbourLeft = f5;
+            f4.NeighbourLeft = f3;
+            f5.NeighbourLeft = f2;
+            f6.NeighbourLeft = f2;
+
+            f1.NeighbourRight = f3;
+            f2.NeighbourRight = f1;
+            f3.NeighbourRight = f1;
+            f4.NeighbourRight = f6;
+            f5.NeighbourRight = f4;
+            f6.NeighbourRight = f4;
         }
 
-        /// <summary>
-        /// Returns the neighbours of a given node
-        /// </summary>
-        /// <param name="node">The node to look up</param>
-        /// <returns>The neighbours of the node</returns>
-        public Tuple<T, T, T, T> GetNeighbours(T node)
+        public List<T> GetNeighboursUp(T node)
+        {
+            return null;
+        }
+
+        public List<T> GetNeighboursRight(T node)
+        {
+            return null;
+        }
+
+        public List<T> GetNeighboursDown(T node)
+        {
+            return null;
+        }
+
+        public List<T> GetNeighboursLeft(T node)
         {
             return null;
         }
@@ -36,6 +87,31 @@ namespace Jaws.Data
         /// <param name="node">The node to split</param>
         public void Split(T node)
         {
+
+        }
+
+        public IEnumerable<T> GetArea(T child, int depth)
+        {
+            CircularQuadNode node = nodes[child];
+            if (node.Depth < depth)
+                throw new ArgumentOutOfRangeException("Depth is lower than child depth");
+
+            while (node.Depth > depth)
+                node = node.Parent;
+
+            return GetArea(node, depth);
+        }
+
+        protected IEnumerable<T> GetArea(CircularQuadNode node, int depth)
+        {
+            if(node == null)
+                return new T[]{};
+
+            return new T[] { node.Value }
+                .Concat(GetArea(node.NeighbourUp, depth + 1))
+                .Concat(GetArea(node.NeighbourRight, depth + 1))
+                .Concat(GetArea(node.NeighbourDown, depth + 1))
+                .Concat(GetArea(node.NeighbourLeft, depth + 1));
         }
         
         /// <summary>
@@ -55,7 +131,7 @@ namespace Jaws.Data
         /// </summary>
         /// <param name="node">The node to find</param>
         /// <param name="replacement">The node that replaces the found node</param>
-        public void Replace(T node, T replacement)
+        public void Replace(T node, int depth, T replacement)
         {
 
         }
@@ -82,6 +158,8 @@ namespace Jaws.Data
 
         protected class CircularQuadNode
         {
+            public CircularQuadNode Parent { get; set; }
+
             public int Depth { get; set; }
 
             public CircularQuadNode NeighbourUp { get; set; }
@@ -96,9 +174,12 @@ namespace Jaws.Data
             public CircularQuadNode ChildBottomRight { get; set; }
             public CircularQuadNode ChildBottomLeft { get; set; }
 
-            public bool IsLeaf()
+            public bool IsLeaf
             {
-                return ChildTopLeft == null && ChildTopRight == null && ChildBottomRight == null && ChildBottomLeft == null;
+                get
+                {
+                    return ChildTopLeft == null && ChildTopRight == null && ChildBottomRight == null && ChildBottomLeft == null;
+                }
             }
         }
     }
