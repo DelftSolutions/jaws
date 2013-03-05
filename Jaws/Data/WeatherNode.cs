5f48b3ce759f8187462ebe2fd1953ea20595a15f
Derk-Jan Karrenbeld <derk-jan+git@karrenbeld.info>
2013-03-05 15:36:58 +0100

    Added WeatherNode class

---
+++ Jaws/Data/WeatherNode.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jaws.Data
+{
+    public class WeatherNode : IQuadNode, ICloneable
+    {
+        public Single Temperature { get; set; }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public WeatherNode()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public Tuple<IQuadNode, IQuadNode, IQuadNode, IQuadNode> Split()
+        {
+            return new Tuple<IQuadNode, IQuadNode, IQuadNode, IQuadNode>(DeepClone(), DeepClone(), DeepClone(), DeepClone());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public WeatherNode DeepClone()
+        {
+            return new WeatherNode()
+            {
+
+            };
+        }
+
+        Object ICloneable.Clone()
+        {
+            return DeepClone();
+        }
+    }
+}
