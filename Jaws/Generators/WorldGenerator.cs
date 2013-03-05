66229b315b6c5ccc6ea675b02ee86d9cc9799f74
Derk-Jan Karrenbeld <derk-jan+git@karrenbeld.info>
2013-03-05 15:18:46 +0100

    Added WorldGenerator

---
+++ Jaws/Generators/WorldGenerator.cs
@@ -0,0 +1,16 @@
+﻿using Jaws.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jaws.Generators
+{
+    public class WorldGenerator
+    {
+        public static CircularQuadTree<Object> Generate()
+        {
+            return null;
+        } 
+    }
+}
